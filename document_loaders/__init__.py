3f78bf66602f8c6353eca913040e5718e3dc6029
zollty <zollty@163.com>
2024-06-07 10:22:33 +0800

    update

--- document_loaders/__init__.py
+++ document_loaders/__init__.py
@@ -1,5 +1,5 @@
-# from .mypdfloader import RapidOCRPDFLoader
-# from .myimgloader import RapidOCRLoader
-# from .mydocloader import RapidOCRDocLoader
-# from .mypptloader import RapidOCRPPTLoader
+from .mypdfloader import RapidOCRPDFLoader
+from .myimgloader import RapidOCRLoader
+from .mydocloader import RapidOCRDocLoader
+from .mypptloader import RapidOCRPPTLoader
 from .FilteredCSVloader import FilteredCSVLoader
