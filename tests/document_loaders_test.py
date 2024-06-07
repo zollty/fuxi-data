07760a753df10395a5fa5bf45697b28980711a3d
zollty <zollty@163.com>
2024-06-07 10:15:38 +0800

    update

--- tests/document_loaders_test.py
+++ tests/document_loaders_test.py
@@ -4,8 +4,8 @@ import os
 # 获取当前脚本的绝对路径
 __current_script_path = os.path.abspath(__file__)
 # 将项目根目录添加到sys.path
-get_runtime_root_dir = os.path.dirname(os.path.dirname(__current_script_path))
-sys.path.append(get_runtime_root_dir)
+runtime_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__current_script_path)))
+sys.path.append(runtime_root_dir)
 
 if __name__ == "__main__":
     from deep_parser.core.document_loaders_helper import load_file_docs
